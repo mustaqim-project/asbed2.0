11efa94edfbc2de1702245e7c988f99ec340bbbd
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-09-23 17:17:53 +0700

    sad234234

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,36 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Detection\MobileDetect;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
-    return view('welcome');
+    $detect = new MobileDetect;
+
+    if ($detect->isMobile()) {
+        return view('mobile.frontend.dashboard.index');
+    } elseif ($detect->isTablet()) {
+        return view('mobile.frontend.dashboard.index');
+    } else {
+        return view('desktop.frontend.home-components.home');
+    }
 });
+
+
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    $detect = new MobileDetect;
+
+    if ($detect->isMobile() || $detect->isTablet()) {
+        return view('mobile.frontend.dashboard.index');
+    } else {
+        return view('dashboard');
+    }
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+
 
 require __DIR__.'/auth.php';
+
+
