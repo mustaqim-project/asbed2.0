2d7c48706ab826c9d8a78ca2e762f157713c1b41
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-09-23 16:19:51 +0700

    dfasdf345

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Detection\MobileDetect;
 
 
 Route::get('/', function () {
-    $detect = new MobileDetect;
+    $detect = new MobileDetect();
 
     if ($detect->isMobile()) {
         return view('mobile.frontend.dashboard.index');
